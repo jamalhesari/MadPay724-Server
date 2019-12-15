6f3de9447e73e79108d0d527d316bd11a04544a3
Jamal <Jamal@THINKPAD-S430>
2019-12-15 19:22:06 +0330

    init Project

---
+++ MadPay724.Presentation/Controllers/ValueController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadPay724.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        // GET: api/Value
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Value/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Value
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return null;
+        }
+
+        // PUT: api/Value/5
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id, [FromBody] string value)
+        {
+            return null;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            return null;
+        }
+    }
+}
